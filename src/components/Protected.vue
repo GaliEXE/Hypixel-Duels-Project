298be31478eb68b6464d7a45caea99c0b520216b
Gryphon Miller <95644173+GaliEXE@users.noreply.github.com>
2021-12-13 09:14:20 -0600

    Add files via upload

---
+++ src/components/Protected.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Bravo! You made it! Protected Page</h1>
+</template>
+
+<script>
+  import swal from "sweetalert2";
+  export default {
+    name: "Protected",
+    beforeRouteEnter: (to, from, next) => {
+      next(vm => {
+        if (vm.$store.state.user.user) {
+          return next();
+        } else {
+          swal({
+            title: "Error!",
+            text: "Please log in or sign up!",
+            type: "error",
+            allowOutsideClick: false,
+            confirmButtonText: "All Right"
+          });
+          return next({ name: "Home" });
+        }
+      });
+    }
+  };
+</script>
