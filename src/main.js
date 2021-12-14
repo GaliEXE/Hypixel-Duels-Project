913c6171e284fcf1c1ecbc05b90aad11b1913be5
Gryphon Miller <95644173+GaliEXE@users.noreply.github.com>
2021-12-14 10:29:32 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -37,13 +37,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
         // Perform your AJAX/Fetch login
 
-        setFormMessage(loginForm, "error", "Invalid username/password combination");
+        setFormMessage(loginForm, "error", "Invalid username or password");
     });
 
     document.querySelectorAll(".form__input").forEach(inputElement => {
         inputElement.addEventListener("blur", e => {
-            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
-                setInputError(inputElement, "Username must be at least 10 characters in length");
+            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 4) {
+                setInputError(inputElement, "Username must be at least 4 characters in length");
             }
         });
 
